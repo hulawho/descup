f774cf27e40f6e05ae469108c4666dc4808d39bc
Umran Hussein <umran.hussein@gmail.com>
2016-02-20 00:03:29 -0800

    preliminaries

---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<title>DES Cup | Home</title>
+		<link rel="stylesheet" href="/public/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/public/bootstrap/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="/public/stylesheets/alt.css">
+		<link rel="stylesheet" href="/public/fonts/lato/lato.css">
+		<script type="text/javascript" src="/public/javascript/jquery.min.js"></script>
+		<script type="text/javascript" src="/public/bootstrap/js/bootstrap.min.js"></script>
+	</head>
+
+<body>
