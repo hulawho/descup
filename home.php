9ca847473ee14d451c030fac972ad180f925410a
Umran Hussein <umran.hussein@gmail.com>
2016-02-20 23:58:26 -0800

    touchups

--- home.php
+++ home.php
@@ -29,25 +29,15 @@
 				</div>
 				<div class='row'>
 					<div class='col-xs-12 col-md-12 padding-top'>
-						<div class='panel'>
-							<table class="table">
-								<thead>
-									<tr>
-										<th>#</th><th>Player</th><th>Team</th><th>Goals</th><th>Assists</th>
-									</tr> 
-								</thead>
-								<tbody>
-									<tr>
-										<th scope="row">1</th><td>Mesut Özil</td><td>Arsenal</td><td>17</td><td>5</td> 
-									</tr> 
-									<tr>
-										<th scope="row">2</th><td>Santi Cazorla</td><td>Arsenal</td><td>15</td><td>7</td>
-									</tr>
-									<tr>
-										<th scope="row">3</th><td>Francis Coquelin</td><td>Arsenal</td><td>13</td><td>7</td>
-									</tr>
-								</tbody>
-							</table>
+						<div class='panel panel-default'>
+							<div class='panel-heading'>
+								<h3>Recent results</h3>
+							</div>
+
+							<div class='panel-body'>
+								<p>Arsenal vs. Leicester City: 2 - 1</p>
+								<p>Tottenham Hotspur vs. Westbromwich Albion: 4 - 2</p>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -78,15 +68,25 @@
 						</div>
 					</div>
 					<div class='col-xs-12 col-md-12 padding-top'>
-						<div class='panel panel-default'>
-							<div class='panel-heading'>
-								<h3>Recent results</h3>
-							</div>
-
-							<div class='panel-body'>
-								<p>Arsenal vs. Leicester City: 2 - 1</p>
-								<p>Tottenham Hotspur vs. Westbromwich Albion: 4 - 2</p>
-							</div>
+						<div class='panel'>
+							<table class="table">
+								<thead>
+									<tr>
+										<th>#</th><th>Player</th><th>Team</th><th>Goals</th><th>Assists</th>
+									</tr> 
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">1</th><td>Mesut Özil</td><td>Arsenal</td><td>17</td><td>5</td> 
+									</tr> 
+									<tr>
+										<th scope="row">2</th><td>Santi Cazorla</td><td>Arsenal</td><td>15</td><td>7</td>
+									</tr>
+									<tr>
+										<th scope="row">3</th><td>Francis Coquelin</td><td>Arsenal</td><td>13</td><td>7</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
