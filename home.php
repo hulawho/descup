cd8b2886a9eb4b4128cd73b75a4d75713839ae81
Umran Hussein <umran.hussein@gmail.com>
2016-02-20 20:20:57 -0800

    all necessary components added

--- home.php
+++ home.php
@@ -1,56 +1,94 @@
 	<div class='row-fluid'>
 		<div class='container'>
 			<div class='col-xs-12 col-md-6 post'>
-				<div class='panel panel-nb'>
-					<div class='row'>
-						<div class='container'>
-							<div class='col-xs-12 col-md-3 panel-container'>
-								<div class='panel panel-default'>
-									<div class='panel-heading'>
-										<h3>Today's games</h3>
-									</div>
-				
-									<div class='panel-body'>
-										<p>Arsenal vs. Barcelona</p>
-									</div>
+				<div class='row'>
+					<div class='panel panel-nb'>
+						<div class='col-xs-12 col-md-6 panel-first'>
+							<div class='panel panel-default'>
+								<div class='panel-heading'>
+									<h3>Today's games</h3>
+								</div>
+			
+								<div class='panel-body'>
+									<p>Arsenal vs. Barcelona</p>
 								</div>
 							</div>
-							<div class='col-xs-12 col-md-3 panel-container'>
-								<div class='panel panel-default'>
-									<div class='panel-heading'>
-										<h3>Upcoming Games</h3>
-									</div>
-				
-									<div class='panel-body'>
-										<p>Leicester City vs. Manchester United</p>
-									</div>
+						</div>
+						<div class='col-xs-12 col-md-6 panel-last'>
+							<div class='panel panel-default'>
+								<div class='panel-heading'>
+									<h3>Upcoming Games</h3>
+								</div>
+			
+								<div class='panel-body'>
+									<p>Leicester City vs. Manchester United</p>
 								</div>
 							</div>
+						</div>
+					</div>
+				</div>
+				<div class='row'>
+					<div class='col-xs-12 col-md-12 padding-top'>
+						<div class='panel'>
+							<table class="table">
+								<thead>
+									<tr>
+										<th>#</th><th>Player</th><th>Team</th><th>Goals</th><th>Assists</th>
+									</tr> 
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">1</th><td>Mesut Özil</td><td>Arsenal</td><td>17</td><td>5</td> 
+									</tr> 
+									<tr>
+										<th scope="row">2</th><td>Santi Cazorla</td><td>Arsenal</td><td>15</td><td>7</td>
+									</tr>
+									<tr>
+										<th scope="row">3</th><td>Francis Coquelin</td><td>Arsenal</td><td>13</td><td>7</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
 			</div>
 		
 			<div class='col-xs-12 col-md-6 post post-next'>
-				<div class='panel'>
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>#</th><th>Team</th><th>GP</th><th>W</th><th>D</th><th>L</th><th>GD</th><th>PTS</th>
-							</tr> 
-						</thead>
-						<tbody>
-							<tr>
-								<th scope="row">1</th><td>Arsenal</td><td>23</td><td>17</td><td>5</td><td>1</td><td>23</td><td>56</td> 
-							</tr> 
-							<tr>
-								<th scope="row">2</th><td>Leicester City</td><td>23</td><td>15</td><td>7</td><td>1</td><td>23</td><td>52</td> 
-							</tr>
-							<tr>
-								<th scope="row">3</th><td>Manchester City</td><td>23</td><td>13</td><td>7</td><td>3</td><td>23</td><td>46</td> 
-							</tr>
-						</tbody>
-					</table>
+				<div class='row'>
+					<div class='col-xs-12 col-md-12'>
+						<div class='panel'>
+							<table class="table">
+								<thead>
+									<tr>
+										<th>#</th><th>Team</th><th>GP</th><th>W</th><th>D</th><th>L</th><th>GD</th><th>PTS</th>
+									</tr> 
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">1</th><td>Arsenal</td><td>23</td><td>17</td><td>5</td><td>1</td><td>23</td><td>56</td> 
+									</tr> 
+									<tr>
+										<th scope="row">2</th><td>Leicester City</td><td>23</td><td>15</td><td>7</td><td>1</td><td>23</td><td>52</td> 
+									</tr>
+									<tr>
+										<th scope="row">3</th><td>Manchester City</td><td>23</td><td>13</td><td>7</td><td>3</td><td>23</td><td>46</td> 
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div class='col-xs-12 col-md-12 padding-top'>
+						<div class='panel panel-default'>
+							<div class='panel-heading'>
+								<h3>Recent results</h3>
+							</div>
+
+							<div class='panel-body'>
+								<p>Arsenal vs. Leicester City: 2 - 1</p>
+								<p>Tottenham Hotspur vs. Westbromwich Albion: 4 - 2</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		
